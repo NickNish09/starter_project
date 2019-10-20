d125ce4f6b25caa5b08210ba778ef11cdeb7ba3f
nicknish09 <nnmarques97@gmail.com>
2019-10-20 13:45:22 -0300

    seed com admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Criando admin..."
+user = User.create(email: "admin@admin.com", first_name: "Admin", last_name: "Admin",
+                   password: "123456"
+)
+user.add_role :admin
+puts "Admin criado."
