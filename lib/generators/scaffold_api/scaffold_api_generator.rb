8304e4127616f1ecfa7ef252f0d532a6a907d7df
nicknish09 <nnmarques97@gmail.com>
2019-10-20 19:01:07 -0300

    scaffold_api started

---
+++ lib/generators/scaffold_api/scaffold_api_generator.rb
@@ -0,0 +1,19 @@
+class ScaffoldApiGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('templates', __dir__)
+
+  argument :methods, type: :array, default: [], banner: "method method"
+  class_option :module, type: :string
+
+  def generate_scaffold_api
+    @module_name = options[:module]
+
+    api_dir_path = "app/controllers/v1"
+    generator_dir_path = api_dir_path + ("/#{@module_name.underscore}" if @module_name.present?).to_s
+    generator_path = generator_dir_path + "/#{file_name}_controller.rb"
+
+    Dir.mkdir(api_dir_path) unless File.exist?(api_dir_path)
+    Dir.mkdir(generator_dir_path) unless File.exist?(generator_dir_path)
+
+    template "api.rb.tt", generator_path
+  end
+end
