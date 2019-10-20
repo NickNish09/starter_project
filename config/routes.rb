72e20b61b54df3c9357162dcc209cb94cf5a9838
nicknish09 <nnmarques97@gmail.com>
2019-10-20 16:52:25 -0300

    updated scaffold index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
   root to: 'main#index'
   devise_for :users, controllers: {
       registrations: 'users/registrations',
   }
-  resources :posts
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20191020152914_remove_description_from_posts.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDescriptionFromPosts < ActiveRecord::Migration[6.0]
-  def change
-
-    remove_column :posts, :description, :text
-  end
-end
